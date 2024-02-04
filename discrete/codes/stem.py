f0012abeef3654f8d8b27440f5d5e0e602546d55
Ashley-Ann-Benoy <155824405+Ashley-Ann-Benoy@users.noreply.github.com>
2024-02-04 23:08:14 +0530

    Update stem.py

--- discrete/codes/stem.py
+++ discrete/codes/stem.py
@@ -1,24 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def read_points_from_file(file_path):
-    data = np.loadtxt('output.dat')
-    x_values = data[:, 0]
-    y_values = data[:, 1]
-    return x_values, y_values
+# Specify file path
+file_path = 'output.dat'
 
-def plot_points(x_values, y_values):
-    plt.stem(x_values, y_values, linefmt='b-', markerfmt='bo', basefmt='r-')
-    plt.ylabel('x(n)')
-    plt.xlabel('n')
-    plt.grid(True)
-    plt.title('Stem Plot of x(n)=(5n+1)u(n)')
-    plt.show()
+# Read data from the file
+data = np.loadtxt(file_path)
+x_values, y_values = data[:, 0], data[:, 1]
 
-def main():
-    file_path = 'your_file.txt'  # Replace with the path to your text file
-    x_values, y_values = read_points_from_file(file_path)
-    plot_points(x_values, y_values)
-
-if __name__ == "__main__":
-    main()
+# Plot the data
+plt.stem(x_values, y_values, linefmt='b-', markerfmt='bo', basefmt='r-')
+plt.ylabel('x(n)')
+plt.xlabel('n')
+plt.grid(True)
+plt.title('Stem Plot of x(n) = (5n + 1)u(n)')
+plt.show()
