9ee98e60c7884f02ed47d6689722aba0326eb839
Ashley-Ann-Benoy <155824405+Ashley-Ann-Benoy@users.noreply.github.com>
2024-02-14 17:09:41 +0530

    Update output.c

--- discrete/codes/output.c
+++ discrete/codes/output.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    FILE *file = fopen("output.dat", "w");  // Open a text file for writing
-
+    FILE *file = fopen("sumval.dat", "w");  
     if (file == NULL) {
         printf("Error opening the file.\n");
         return 1;  // Return an error code
     }
 
-    // Use fprintf to write values to the file directly without arrays
+    
     for (int i = 0; i < 10; i++) {
         int x = i;
-        float y =1+5*x;
+        float y =i+1 2.5*i*(i-1);
+        fprintf(file, "%d %.2f\n", x, y);
+    }
+
+    fclose(file);  // Close the file
+
+
+     FILE *file = fopen("sumap.dat", "w");  
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        return 1;  // Return an error code
+    }
+
+    
+    for (int i = 0; i < 10; i++) {
+        int x = i
+        float y =i*(3.5+2.5*i);
         fprintf(file, "%d %.2f\n", x, y);
     }
 
