69d3a6befd6b3ef43b66c00869abfae071206a0a
Ashley-Ann-Benoy <155824405+Ashley-Ann-Benoy@users.noreply.github.com>
2024-02-23 23:35:10 +0530

    Add files via upload

---
+++ gate/codes/code_gate.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+# System parameters
+a = 2
+beta = 4
+
+# Transfer function of the lead compensator
+numerator = [1, a]
+denominator = [1, a * beta]
+system = signal.TransferFunction(numerator, denominator)
+
+# Frequency range for Bode plot
+frequency = np.logspace(-1, 2, 1000)
+
+# Bode plot calculation
+frequency_response = signal.bode(system, frequency)
+
+# Plotting the Bode plots
+plt.figure(figsize=(10, 6))
+
+# Amplitude plot
+plt.subplot(2, 1, 1)
+plt.semilogx(frequency, frequency_response[0])
+plt.title('Bode Plot - Amplitude')
+plt.xlabel('Frequency (rad/s)')
+plt.ylabel('Amplitude (dB)')
+plt.grid(True)
+
+# Phase plot
+plt.subplot(2, 1, 2)
+plt.semilogx(frequency, frequency_response[1])
+plt.title('Bode Plot - Phase')
+plt.xlabel('Frequency (rad/s)')
+plt.ylabel('Phase (degrees)')
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
+
